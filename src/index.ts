f65817be714078630e74ace92c4c0df8f7ce8065
Hristo Kolev <hristo.kolev@tick42.com>
2023-04-03 02:31:27 +0300

    reset

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,34 @@
-import { addDays, format, subMonths } from 'date-fns';
-import { utcToZonedTime, zonedTimeToUtc } from 'date-fns-tz';
-
 /**
  * Format the short date in the current timezone.
  */
-export const exercise1 = (date: string): string =>
-  format(new Date(`${date}T00:00`), 'dd MMM yyyy');
+export const exercise1 = (date: string): string => {
+  throw new Error('Not implemented.');
+};
 
 /**
  * Format the UTC date in the current timezone.
  */
-export const exercise2 = (date: string): string =>
-  format(new Date(date), 'dd MMM yyyy HH:mm:ss');
+export const exercise2 = (date: string): string => {
+  throw new Error('Not implemented.');
+};
 
 /**
  * Format the NY date in UK timezone.
  */
-export const exercise3 = (date: string): string =>
-  format(
-    utcToZonedTime(
-      zonedTimeToUtc(new Date(date), 'America/New_York'),
-      'Europe/London'
-    ),
-    'dd MMM yyyy HH:mm:ss'
-  );
+export const exercise3 = (date: string): string => {
+  throw new Error('Not implemented.');
+};
 
 /**
  * Add 2 days to the date and format the result in the current timezone.
  */
-export const exercise4 = (date: string): string =>
-  format(addDays(new Date(date), 2), 'dd MMM yyyy HH:mm:ss');
+export const exercise4 = (date: string): string => {
+  throw new Error('Not implemented.');
+};
 
 /**
  * Subtract 4 months from the date and format the result in the current timezone.
  */
-export const exercise5 = (date: string): string =>
-  format(subMonths(new Date(date), 4), 'dd MMM yyyy HH:mm:ss');
+export const exercise5 = (date: string): string => {
+  throw new Error('Not implemented.');
+};
